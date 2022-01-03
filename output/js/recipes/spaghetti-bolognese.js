f7bdc57c35c304c01526064fb969ce1105f84887
stanton-gary <sgary@hotmail.co.uk>
2022-01-03 21:58:53 +0000

    Added spaghetti bolognese.
    Try out ads.

---
+++ output/js/recipes/spaghetti-bolognese.js
@@ -0,0 +1,87 @@
+{
+    "@context": "http://schema.org/",
+        "@type": "Recipe",
+            "name": "Spaghetti Bolognese",
+                "image": [
+                    "https://recipe-den.web.app/img/4x3/spaghetti-bolognese.jpg",
+                    "https://recipe-den.web.app/img/1x1/spaghetti-bolognese.jpg",
+                    "https://recipe-den.web.app/img/16x9/spaghetti-bolognese.jpg"
+                ],
+                    "author": {
+            "@type": "Person",
+            "name": "Gary Stanton"
+    },
+    "datePublished": "2022-01-03",
+        "description": "Simple spaghetti bolognese with bacon",
+            "prepTime": "PT5M",
+                "cookTime": "PT25M",
+                    "totalTime": "PT30M",
+                        "recipeYield": "3",
+                            "recipeCategory": "Dinner",
+                                "recipeCuisine": "Italian",
+                                    "keywords": "quick, simple, spaghetti, bolognese",
+                                        "recipeIngredient": [
+                                            "6 rashers streaky smoked bacon",
+                                            "2 onions",
+                                            "500g beef mince",
+                                            "400g chopped tomatoes",
+                                            "1 tsp basil",
+                                            "1 tsp rosemary",
+                                            "1 tsp red wine",
+                                            "1 heaped tsp garlic",
+                                            "1 beef stock cube",
+                                            "2 tablespoons tomato puree",
+                                            "200g spaghetti"
+                                        ],
+                                            "recipeInstructions": [
+                                                {
+                                                    "@type": "HowToStep",
+                                                    "text": "Add olive oil to pan and cook bacon for a few minutes.",
+                                                    "url":"https://recipe-den.web.app/recipes/spaghetti-bolognese#step1",
+                                                    "image":"https://recipe-den.web.app/img/spaghetti-bolognese/spaghetti-bolognese.jpg",
+                                                    "name":"Oven"
+                                                },
+                                                {
+                                                    "@type": "HowToStep",
+                                                    "text": "Add in the onions and cook until soft.",
+                                                    "url":"https://recipe-den.web.app/recipes/spaghetti-bolognese#step2",
+                                                    "image":"https://recipe-den.web.app/img/spaghetti-bolognese/spaghetti-bolognese.jpg",
+                                                    "name":"2"
+                                                },
+                                                {
+                                                    "@type": "HowToStep",
+                                                    "text": "Add in the mince and cook until browned all over.",
+                                                    "url":"https://recipe-den.web.app/recipes/spaghetti-bolognese#step3",
+                                                    "image":"https://recipe-den.web.app/img/spaghetti-bolognese/spaghetti-bolognese.jpg",
+                                                    "name":"3"
+                                                },
+                                                {
+                                                    "@type": "HowToStep",
+                                                    "text": "Add in the garlic and cook for a couple of mins.",
+                                                    "url":"https://recipe-den.web.app/recipes/spaghetti-bolognese#step4",
+                                                    "image":"https://recipe-den.web.app/img/spaghetti-bolognese/spaghetti-bolognese.jpg",
+                                                    "name":"4"
+                                                },
+                                                {
+                                                    "@type": "HowToStep",
+                                                    "text": "Add in the chopped tomatoes, herbs, puree and stock cube, turn down heat.",
+                                                    "url":"https://recipe-den.web.app/recipes/spaghetti-bolognese#step5",
+                                                    "image":"https://recipe-den.web.app/img/spaghetti-bolognese/spaghetti-bolognese.jpg",
+                                                    "name":"5"
+                                                },
+                                                {
+                                                    "@type": "HowToStep",
+                                                    "text": "Pop the lid on the pan and simmer for around 20 mins.",
+                                                    "url":"https://recipe-den.web.app/recipes/spaghetti-bolognese#step6",
+                                                    "image":"https://recipe-den.web.app/img/spaghetti-bolognese/spaghetti-bolognese.jpg",
+                                                    "name":"6"
+                                                },
+                                                {
+                                                    "@type": "HowToStep",
+                                                    "text": "Cook the spaghetti and serve.",
+                                                    "url":"https://recipe-den.web.app/recipes/spaghetti-bolognese#step7",
+                                                    "image":"https://recipe-den.web.app/img/spaghetti-bolognese/spaghetti-bolognese.jpg",
+                                                    "name":"7"
+                                                }
+                                            ]
+}
